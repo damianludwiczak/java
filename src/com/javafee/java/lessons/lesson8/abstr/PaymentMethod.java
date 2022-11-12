73031940634bfa5c2cd88c705451ba99da8459bc
Damian Ludwiczak <damian.ludwiczak@grafen.com>
2022-11-12 17:39:42 +0100

    lesson 8

---
+++ src/com/javafee/java/lessons/lesson8/abstr/PaymentMethod.java
@@ -0,0 +1,5 @@
+package com.javafee.java.lessons.lesson8.abstr;
+
+public abstract class PaymentMethod {
+    public abstract void pay(double amount);
+}
---
+++ src/com/javafee/java/lessons/lesson8/finterf/PaymentMethod.java
@@ -0,0 +1,5 @@
+package com.javafee.java.lessons.lesson8.finterf;
+
+public interface PaymentMethod {
+    void pay(double amount);
+}
